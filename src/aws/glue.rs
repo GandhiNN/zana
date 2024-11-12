ec626435d387762267e415da67e589eb2042b49b
Ngakan Gandhi <ngandhi@pmintl.net>
2024-11-12 13:09:41 +0700

    v1 of list tables

--- src/aws/glue.rs
+++ src/aws/glue.rs
@@ -1,6 +1,4 @@
-use aws_sdk_glue::error::SdkError;
-use aws_sdk_glue::operation::get_tables::GetTablesError;
-use aws_sdk_glue::{operation::get_tables::GetTablesOutput, Client, Error};
+use aws_sdk_glue::{Client, Error};
 use aws_types::SdkConfig;
 
 async fn set_client(config: SdkConfig) -> Result<Client, Error> {
@@ -8,15 +6,34 @@ async fn set_client(config: SdkConfig) -> Result<Client, Error> {
     Ok(client)
 }
 
-async fn get_tables(
-    client: &Client,
-    database: String,
-) -> Result<GetTablesOutput, SdkError<GetTablesError>> {
-    client.get_tables().database_name(database).send().await
-}
-
 pub async fn list_tables(config: SdkConfig, database: String) {
     let client = set_client(config).await.unwrap();
-    let tables = get_tables(&client, database).await;
-    println!("{:#?}", tables);
+    let mut list_tables = client
+        .get_tables()
+        .database_name(database)
+        .into_paginator()
+        .send();
+    while let Some(list_tables_output) = list_tables.next().await {
+        match list_tables_output {
+            Ok(list_tables) => {
+                let names = list_tables.table_list();
+                println!("{:#?}", names)
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+}
+
+pub async fn list_jobs(config: SdkConfig) {
+    let client = set_client(config).await.unwrap();
+    let mut list_jobs = client.list_jobs().into_paginator().send();
+    while let Some(list_jobs_output) = list_jobs.next().await {
+        match list_jobs_output {
+            Ok(list_jobs) => {
+                let names = list_jobs.job_names();
+                println!("{:#?}", names)
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
 }
