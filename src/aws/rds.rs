5ef38b7a1bb281f595cf7074a95a4f63b3bb7b55
Ngakan Gandhi <ngandhi@pmintl.net>
2024-11-12 11:10:44 +0700

    scaffold rds logic

--- src/aws/rds.rs
+++ src/aws/rds.rs
@@ -1,14 +1,29 @@
-use aws_sdk_rds::{Client, Error};
+use aws_sdk_rds::{operation::describe_db_instances::DescribeDbInstancesOutput, Client, Error};
+use aws_types::SdkConfig;
 
-pub async fn show_instances(client: &Client) -> Result<(), Error> {
-    let instances = client.describe_db_instances().send().await?;
-    for instance in instances.db_instances() {
-        println!(
-            "DB instance identifier: {:?}",
-            instance
-                .db_instance_identifier()
-                .expect("Instance should have identifiers")
-        )
+async fn set_client(config: SdkConfig) -> Result<Client, Error> {
+    let client = Client::new(&config);
+    Ok(client)
+}
+
+async fn get_instances(client: &Client) -> Result<DescribeDbInstancesOutput, Error> {
+    Ok(client.describe_db_instances().send().await?)
+}
+
+pub async fn list_instances(config: SdkConfig) {
+    let client = set_client(config).await.unwrap();
+    let instances_list = get_instances(&client).await;
+    match instances_list {
+        Ok(instances) => {
+            for instance in instances.db_instances() {
+                println!(
+                    "DB instance identifier: {:?}",
+                    instance
+                        .db_instance_identifier()
+                        .expect("Instance should have identifiers")
+                )
+            }
+        }
+        Err(e) => println!("{:?}", e),
     }
-    Ok(())
 }
