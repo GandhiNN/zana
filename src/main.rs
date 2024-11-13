8dca706f1a951ffabdb02d0ea914269bd3ca445d
Ngakan Gandhi <ngandhi@pmintl.net>
2024-11-13 16:04:04 +0700

    add log builder

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
-use log::{error, info};
+use chrono::Local;
+use env_logger::Builder;
+use log::LevelFilter;
+use std::io::Write;
 use zana::aws::{config, dynamodb, glue, rds};
 
 #[tokio::main]
 async fn main() {
+    // Setup logger
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] = {}",
+                Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+
     // Read from CLI arguments
     let matches = zana::cli::cmd().get_matches();
 
     // Parse global arguments to propagate to subcommands
     let aws_profile = matches.get_one::<String>("profile").unwrap();
     let timeout: &u64 = matches.get_one::<u64>("timeout").unwrap();
-    info!("Using shared config with profile name: {}", aws_profile);
+    log::info!("Using shared config with profile name: {}", aws_profile);
     let shared_config: aws_types::SdkConfig = config::set_config(aws_profile, *timeout).await;
 
     // Match subcommands input
@@ -33,15 +50,15 @@ async fn main() {
         // AWS RDS logic
         match matches.get_one::<String>("task").unwrap().as_str() {
             "list-instances" => rds::list_instances(shared_config).await,
-            _ => error!("No task provided!"),
+            _ => log::error!("No task provided!"),
         }
     } else if let Some(matches) = matches.subcommand_matches("dynamodb") {
         // AWS DynamoDB logic
         match matches.get_one::<String>("task").unwrap().as_str() {
             "list-tables" => dynamodb::list_tables(shared_config).await,
-            _ => error!("No task provided!"),
+            _ => log::error!("No task provided!"),
         }
     } else {
-        error!("Faulty input is provided!")
+        log::error!("Faulty input is provided!")
     }
 }
