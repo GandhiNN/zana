897bf53e0563bb9a569544d0277b9688bef70858
Ngakan Gandhi <ngandhi@pmintl.net>
2024-11-13 11:01:56 +0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -6,39 +6,27 @@ async fn main() {
     // Read from CLI arguments
     let matches = zana::cli::cmd().get_matches();
 
+    // Parse global arguments to propagate to subcommands
+    let aws_profile = matches.get_one::<String>("profile").unwrap();
+    let timeout: &u64 = matches.get_one::<u64>("timeout").unwrap();
+    println!("Using shared config with profile name: {}", aws_profile);
+    let shared_config: aws_types::SdkConfig = config::set_config(aws_profile, *timeout).await;
+
     // Match subcommands input
     if let Some(matches) = matches.subcommand_matches("glue") {
-        // AWS Glue tasks
-        // Parse arguments
-        let aws_profile = matches.get_one::<String>("profile").unwrap();
-        let timeout: &u64 = matches.get_one::<u64>("timeout").unwrap();
-        // Build aws config
-        println!("Using shared config with profile name: {}", aws_profile);
-        let shared_config: aws_types::SdkConfig = config::set_config(aws_profile, *timeout).await;
+        // AWS Glue logic
         match matches.get_one::<String>("task").unwrap().as_str() {
             "list-tables" => glue::list_tables(shared_config, "lesmes-ro01".to_string()).await,
             _ => println!("No task provided!"),
         }
     } else if let Some(matches) = matches.subcommand_matches("rds") {
-        // AWS RDS tasks
-        // Parse arguments
-        let aws_profile = matches.get_one::<String>("profile").unwrap();
-        let timeout: &u64 = matches.get_one::<u64>("timeout").unwrap();
-        // Build aws config
-        println!("Using shared config with profile name: {}", aws_profile);
-        let shared_config: aws_types::SdkConfig = config::set_config(aws_profile, *timeout).await;
+        // AWS RDS logic
         match matches.get_one::<String>("task").unwrap().as_str() {
             "list-instances" => rds::list_instances(shared_config).await,
             _ => println!("No task provided!"),
         }
     } else if let Some(matches) = matches.subcommand_matches("dynamodb") {
-        // AWS DynamoDB tasks
-        // Parse arguments
-        let aws_profile = matches.get_one::<String>("profile").unwrap();
-        let timeout: &u64 = matches.get_one::<u64>("timeout").unwrap();
-        // Build aws config
-        println!("Using shared config with profile name: {}", aws_profile);
-        let shared_config: aws_types::SdkConfig = config::set_config(aws_profile, *timeout).await;
+        // AWS DynamoDB logic
         match matches.get_one::<String>("task").unwrap().as_str() {
             "list-tables" => dynamodb::list_tables(shared_config).await,
             _ => println!("No task provided!"),
