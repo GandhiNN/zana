6850667fb74e5729cbcf2bcb350590699fa96570
Ngakan Gandhi <ngandhi@pmintl.net>
2024-11-14 14:47:32 +0700

    add pretty print logic

--- src/aws/rds.rs
+++ src/aws/rds.rs
@@ -1,6 +1,18 @@
 use aws_sdk_rds::{operation::describe_db_instances::DescribeDbInstancesOutput, Client, Error};
 use aws_types::SdkConfig;
 
+use tabled::settings::Style;
+use tabled::{Table, Tabled};
+
+#[derive(Tabled, Debug)]
+struct DBInstance {
+    id: String,
+    class: String,
+    engine: String,
+    status: String,
+    endpoint: String,
+}
+
 async fn set_client(config: SdkConfig) -> Result<Client, Error> {
     let client = Client::new(&config);
     Ok(client)
@@ -15,14 +27,25 @@ pub async fn list_instances(config: SdkConfig) {
     let instances_list = get_instances(&client).await;
     match instances_list {
         Ok(instances) => {
-            println!("id,class,engine,status,endpoint");
+            let mut db_instances: Vec<DBInstance> = Vec::new();
+            // println!("id,class,engine,status,endpoint");
             for instance in instances.db_instances() {
-                let id = instance.db_instance_identifier().unwrap();
-                let class = instance.db_instance_class().unwrap();
-                let engine = instance.engine().unwrap();
-                let status = instance.db_instance_status().unwrap();
-                let endpoint = instance.endpoint().unwrap().address.clone().unwrap();
-                println!("{},{},{},{},{}", id, class, engine, status, endpoint);
+                let db_id = instance.db_instance_identifier().unwrap().to_string();
+                let db_class = instance.db_instance_class().unwrap().to_string();
+                let db_engine = instance.engine().unwrap().to_string();
+                let db_status = instance.db_instance_status().unwrap().to_string();
+                let db_endpoint = instance.endpoint().unwrap().address.clone().unwrap();
+                // println!("{},{},{},{},{}", id, class, engine, status, endpoint);
+                db_instances.push(DBInstance {
+                    id: db_id,
+                    class: db_class,
+                    engine: db_engine,
+                    status: db_status,
+                    endpoint: db_endpoint,
+                });
+                let mut table = Table::new(&db_instances);
+                table.with(Style::psql());
+                println!("{}", table);
             }
         }
         Err(e) => println!("{:?}", e),
