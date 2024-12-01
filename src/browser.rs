5ec2fc050a2cceebd7ab2e625218dbcbdcd3cd20
Ngakan Gandhi <ngandhi@pmintl.net>
2024-12-01 18:48:39 +0700

    move out util module from common

--- src/browser.rs
+++ src/browser.rs
@@ -1,4 +1,4 @@
-use crate::common::util;
+use crate::util;
 use ini::ini;
 use std::collections::HashMap;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 pub mod aws;
 pub mod browser;
 pub mod cli;
-pub mod common;
+pub mod util;
