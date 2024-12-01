ce4d624f68addc375246f4dde586380cb35f59e9
Ngakan Gandhi <ngandhi@pmintl.net>
2024-12-01 14:18:26 +0700

    add styled printing

--- src/aws/s3.rs
+++ src/aws/s3.rs
@@ -1,5 +1,14 @@
 use aws_sdk_s3::{Client, Error};
 use aws_types::SdkConfig;
+use tabled::settings::Style;
+use tabled::{Table, Tabled};
+
+#[derive(Tabled, Debug)]
+struct S3Bucket {
+    name: String,
+    created_at: String,
+    region: String,
+}
 
 async fn set_client(config: SdkConfig) -> Result<Client, Error> {
     let client = Client::new(&config);
@@ -12,11 +21,21 @@ pub async fn list_buckets(config: SdkConfig) {
     while let Some(list_buckets_output) = list_buckets.next().await {
         match list_buckets_output {
             Ok(list_buckets) => {
-                let names = list_buckets.buckets();
-                // for name in names {
-                //     println!("{}", name);
-                // }
-                println!("{:?}", names);
+                let mut s3_bucket: Vec<S3Bucket> = Vec::new();
+                let buckets = list_buckets.buckets();
+                for bucket in buckets {
+                    let bucket_name = bucket.name().unwrap().to_string();
+                    let creation_date = bucket.creation_date().unwrap().to_string();
+                    let bucket_region = bucket.bucket_region().unwrap_or("None").to_string();
+                    s3_bucket.push(S3Bucket {
+                        name: bucket_name,
+                        created_at: creation_date,
+                        region: bucket_region,
+                    });
+                }
+                let mut table = Table::new(&s3_bucket);
+                table.with(Style::psql());
+                println!("{}", table);
             }
             Err(e) => println!("{:?}", e),
         }
