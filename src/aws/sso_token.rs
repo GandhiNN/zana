63ad1c59edb65d8d9ab0277267ff143fff968e2b
Ngakan Gandhi <ngandhi@pmintl.net>
2025-02-17 13:50:55 +0700

    checkpoint for sso token

--- src/aws/sso_token.rs
+++ src/aws/sso_token.rs
@@ -1,12 +1,12 @@
 use crate::utils::json;
 use crate::utils::serde::json_date_format;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use aws_config::SdkConfig;
 use aws_sdk_ssooidc::Client;
 use chrono::{DateTime, Duration, TimeZone, Utc};
 use serde::{Deserialize, Serialize};
-use std::fs;
 use std::path::{Path, PathBuf};
+use std::{fs, io::Write};
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -92,5 +92,138 @@ impl SsoAccessTokenProvider {
             client: Client::new(config),
             cache: AccessTokenCache::new(sso_session_name, sso_cache_dir.as_path()),
         })
+    }
+
+    pub async fn get_access_token(&self, start_url: &str) -> Result<AccessToken> {
+        let cached_token_option = self.cache.get_cached_token();
+
+        match cached_token_option {
+            Ok(cached_token) => {
+                if cached_token.is_expired() {
+                    self.get_new_token(start_url).await
+                } else {
+                    self.refresh_token(cached_token).await
+                }
+            }
+            Err(_) => self.get_new_token(start_url).await,
+        }
+    }
+
+    async fn get_new_token(&self, start_url: &str) -> Result<AccessToken> {
+        let device_client = self.register_device_client().await?;
+        self.authenticate(start_url, device_client).await
+    }
+
+    async fn register_device_client(&self) -> Result<DeviceClient, anyhow::Error> {
+        let response = self
+            .client
+            .register_client()
+            .client_name(format!("{}-{}", Self::CLIENT_NAME, self.sso_session_name))
+            .client_type("public")
+            .scopes("sso:account:access")
+            .send()
+            .await?;
+        let client_id = response.client_id().unwrap();
+        let client_secret = response.client_secret().unwrap();
+        let registration_expires_at = Utc
+            .timestamp_opt(response.client_secret_expires_at(), 0)
+            .unwrap();
+        let device_client = DeviceClient {
+            client_id: String::from(client_id),
+            client_secret: String::from(client_secret),
+            registration_expires_at,
+        };
+        Ok(device_client)
+    }
+
+    async fn authenticate(
+        &self,
+        start_url: &str,
+        device_client: DeviceClient,
+    ) -> Result<AccessToken> {
+        let auth_response = self
+            .client
+            .start_device_authorization()
+            .client_id(device_client.client_id.as_str())
+            .client_secret(device_client.client_secret.as_str())
+            .start_url(start_url)
+            .send()
+            .await?;
+        open::that(auth_response.verification_uri_complete().unwrap())?;
+        println!(
+            "\nVerify authorization code: \x1B[36;1m{}\x18[0m",
+            &auth_response.user_code().unwrap()
+        );
+        let interval = auth_response.interval();
+        loop {
+            let token_response = self
+                .client
+                .create_token()
+                .client_id(device_client.client_id.as_str())
+                .client_secret(device_client.client_secret.as_str())
+                .grant_type(Self::DEVICE_GRANT_TYPE)
+                .device_code(auth_response.device_code().unwrap())
+                .send()
+                .await;
+
+            match token_response {
+                Ok(out) => {
+                    let access_token = out.access_token().unwrap();
+                    let refresh_token = out.refresh_token().unwrap();
+                    let expires_at = Utc::now() + Duration::seconds(out.expires_in() as i64);
+
+                    let access_token = AccessToken {
+                        region: self.client.config().region().unwrap().to_string(),
+                        start_url: String::from(start_url),
+                        access_token: String::from(access_token),
+                        expires_at,
+                        device_client,
+                        refresh_token: String::from(refresh_token),
+                    };
+
+                    print!("\x1B[1A");
+                    print!("\x1B[2K");
+                    std::io::stdout().flush().unwrap();
+
+                    break Ok(self.cache.cache_token(access_token)?);
+                }
+                Err(err) => {
+                    let service_error = err.into_service_error();
+                    if service_error.is_access_denied_exception() {
+                        break Err(anyhow!("Access request rejected"));
+                    }
+                    let millis = Duration::seconds(interval as i64);
+                    std::thread::sleep(millis.to_std()?);
+                }
+            }
+        }
+    }
+
+    async fn refresh_token(&self, cached_token: AccessToken) -> Result<AccessToken> {
+        let device_client = &cached_token.device_client;
+        let response = self
+            .client
+            .create_token()
+            .client_id(device_client.client_id.as_str())
+            .client_secret(device_client.client_secret.as_str())
+            .grant_type(Self::REFRESH_GRANT_TYPE)
+            .refresh_token(cached_token.refresh_token.as_str())
+            .send()
+            .await?;
+
+        let access_token = response.access_token().unwrap();
+        let refresh_token = response.refresh_token().unwrap();
+        let expires_at = Utc::now() + Duration::seconds(response.expires_in() as i64);
+
+        let new_access_token = AccessToken {
+            region: self.client.config().region().unwrap().to_string(),
+            start_url: cached_token.start_url.clone(),
+            access_token: String::from(access_token),
+            expires_at,
+            device_client: cached_token.device_client,
+            refresh_token: String::from(refresh_token),
+        };
+
+        self.cache.cache_token(new_access_token)
     }
 }
