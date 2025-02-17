3bd6636ed7ae04921e2a500662d73609ad1fcb46
Ngakan Gandhi <ngandhi@pmintl.net>
2025-02-17 14:06:19 +0700

    checkpoint sso APIs

--- src/aws/sso.rs
+++ src/aws/sso.rs
@@ -1,5 +1,8 @@
 use crate::aws::region;
+use crate::aws::sso_token::AccessToken;
 use anyhow::Result;
+use aws_config::SdkConfig;
+use aws_sdk_sso::Client;
 use inquire::{InquireError, Select, Text};
 use serde::{Deserialize, Serialize};
 use std::fmt;
@@ -29,6 +32,73 @@ impl fmt::Display for SsoConfig {
             "SSO Start URL: {}\nSSO Region: {}",
             self.start_url, self.region
         )
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct AccountInfo {
+    pub account_name: String,
+    pub account_id: String,
+}
+
+impl fmt::Display for AccountInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} - {}", self.account_name, self.account_id)
+    }
+}
+
+pub struct AccountInfoProvider {
+    client: Client,
+}
+
+impl AccountInfoProvider {
+    pub fn new(sdk_config: &SdkConfig) -> Self {
+        AccountInfoProvider {
+            client: Client::new(sdk_config),
+        }
+    }
+
+    pub async fn get_account_list(&self, access_token: &AccessToken) -> Result<Vec<AccountInfo>> {
+        let list_accounts = self
+            .client
+            .list_accounts()
+            .access_token(access_token.access_token.as_str())
+            .max_results(300)
+            .send()
+            .await?;
+
+        let account_infos = list_accounts
+            .account_list()
+            .iter()
+            .map(|account| AccountInfo {
+                account_id: String::from(account.account_id().unwrap()),
+                account_name: String::from(account.account_name().unwrap_or("unknown")),
+            })
+            .collect::<Vec<_>>();
+
+        Ok(account_infos)
+    }
+
+    pub async fn get_roles_for_account(
+        &self,
+        access_token: &AccessToken,
+        account_info: &AccountInfo,
+    ) -> Result<Vec<String>> {
+        let account_roles = self
+            .client
+            .list_account_roles()
+            .access_token(access_token.access_token.as_str())
+            .account_id(account_info.account_id.as_str())
+            .max_results(10)
+            .send()
+            .await?;
+
+        Ok(account_roles
+            .role_list()
+            .iter()
+            .map(|r| r.role_name().unwrap())
+            .map(String::from)
+            .collect::<Vec<_>>())
     }
 }
 
