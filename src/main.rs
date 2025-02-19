4bc78030826555db16c463e2c3f03e0b6291eef0
Ngakan Gandhi <ngandhi@pmintl.net>
2025-02-19 12:32:39 +0700

    credentials as reference to PathBuf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use directories::BaseDirs;
 use tracing::info;
 use tracing_subscriber::fmt as TracingSubscriberFmt;
@@ -14,8 +16,9 @@ async fn main() {
     let base_dirs = BaseDirs::new().unwrap();
     let home_dir = base_dirs.home_dir().to_string_lossy().to_string();
     let default_credentials_path = format!("{}/.aws/credentials", home_dir);
-    let credentials_path = std::env::var("CREDENTIALS_PATH").unwrap_or(default_credentials_path);
+    let credentials_path =
+        PathBuf::from(std::env::var("CREDENTIALS_PATH").unwrap_or(default_credentials_path));
 
     // Handle CLI arguments
-    cli::runner::run(credentials_path.as_str()).await;
+    cli::runner::run(&credentials_path).await;
 }
