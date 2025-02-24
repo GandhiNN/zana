6cf28cf05dc355a17f7ad04441959255c0960108
Ngakan Gandhi <ngandhi@pmintl.net>
2025-02-24 07:19:18 +0700

    move into its own function

--- src/aws/credentials.rs
+++ src/aws/credentials.rs
@@ -52,7 +52,7 @@ impl Prompt {
     pub const CONFIGURE_REGION: &'static str = "Would you like to configure the default region?";
     pub const SELECT_ACCOUNT: &'static str = "Select the account you would like to use:";
     pub const SELECT_PROFILE: &'static str = "Select the profile you would like to use:";
-    pub const INPUT_PROFILE: &'static str = "Enter the profile you would like to use:";
+    pub const INPUT_PROFILE: &'static str = "Enter the profile name you would like to use:";
     pub const SELECT_ROLE: &'static str = "Select the role you would like to assume:";
     pub const CONTINUE_PROGRAM: &'static str = "Would you like to continue?";
 }
@@ -79,7 +79,7 @@ impl AWSCredentials {
     pub async fn load(&mut self, path: &PathBuf, profile: &str) {
         let mut config_reader = Ini::new();
         let config_map = config_reader.load::<PathBuf>(path.into()).unwrap();
-        println!("{:#?}", config_map);
+        // println!("{:#?}", config_map);
         self.region = config_map
             .get(profile)
             .unwrap()
@@ -140,7 +140,7 @@ impl AWSCredentials {
     }
 
     #[allow(clippy::too_many_arguments)]
-    pub fn create_or_update_credentials(
+    pub fn write_to_credentials_file(
         &self,
         profile: &str,
         region: &str,
@@ -245,7 +245,7 @@ impl AWSCredentials {
                     let new_profile = Text::new(Prompt::INPUT_PROFILE).prompt()?;
                     // Write credentials to file
                     println!("Updating credentials file using profile: {}", new_profile);
-                    self.create_or_update_credentials(
+                    self.write_to_credentials_file(
                         new_profile.as_str(),
                         sso_default_config.region.as_str(),
                         selected_account.account_id.as_str(),
@@ -267,66 +267,13 @@ impl AWSCredentials {
             self.create_credentials_file()?;
             let is_configure =
                 Select::new(Prompt::CONFIGURE_CREDENTIALS, vec!["yes", "no"]).prompt()?;
-            info!("Configuring credentials...");
-            let sso_config = Sso::configure_sso()?;
-            info!("Building default config...");
-            let default_config = Sso::default_config(&sso_config)?;
-            info!("Retrieving account info...");
-            let account_info_provider = AccountInfoProvider::new(&default_config);
-            info!("Retrieving session name...");
-            let session_name = session_name(sso_config.start_url.as_str());
-            let token_provider = SsoAccessTokenProvider::new(
-                &default_config,
-                session_name.as_str(),
-                &PathBuf::from(load_cache_file()),
-            )?;
-            info!("Retrieving access token...");
-            let access_token = token_provider
-                .get_access_token(&sso_config.start_url)
-                .await?;
-            info!("Retrieving sso accounts...");
-            let mut sso_accounts = account_info_provider
-                .get_account_list(&access_token)
-                .await?;
-            sso_accounts.sort();
-            let selected_account = Select::new(Prompt::SELECT_ACCOUNT, sso_accounts).prompt()?;
-            let mut roles = account_info_provider
-                .get_roles_for_account(&access_token, &selected_account)
-                .await?;
-            roles.sort();
-            let selected_role = Select::new(Prompt::SELECT_ROLE, roles).prompt()?;
-
-            // Get role credentials
-            let role_credentials = account_info_provider
-                .get_role_credentials(&selected_role, &selected_account, &access_token)
-                .await?;
-            let (mut access_key_id, mut secret_access_key, mut session_token, mut expiration) =
-                (String::new(), String::new(), String::new(), String::new());
-            let _ = role_credentials
-                .role_credentials
-                .into_iter()
-                .map(|x| {
-                    access_key_id = x.access_key_id().unwrap().to_string();
-                    secret_access_key = x.secret_access_key().unwrap().to_string();
-                    session_token = x.session_token().unwrap().to_string();
-                    expiration = x.expiration().to_string();
-                })
-                .collect::<Vec<()>>();
-            let new_profile = Text::new(Prompt::INPUT_PROFILE).prompt()?;
-
-            // Write credentials to file
-            println!("Updating credentials file using profile: {}", new_profile);
-            self.create_or_update_credentials(
-                new_profile.as_str(),
-                sso_config.clone().region.as_str(),
-                selected_account.account_id.as_str(),
-                access_key_id.as_str(),
-                secret_access_key.as_str(),
-                session_token.as_str(),
-                expiration.parse::<i64>().unwrap(),
-            )?;
-            if is_configure == "no" {
+            if is_configure == "yes" {
+                self.create_or_update_credentials().await?;
+            } else if is_configure == "no" {
                 info!("Exiting program.");
+                return Ok(false);
+            } else {
+                info!("Unrecognized input! Exiting program.");
                 return Ok(false);
             }
         }
@@ -375,6 +322,68 @@ impl AWSCredentials {
             .timeout_config(timeout_config)
             .load()
             .await
+    }
+
+    async fn create_or_update_credentials(&self) -> Result<()> {
+        info!("Configuring credentials...");
+        let sso_config = Sso::configure_sso()?;
+        info!("Building default config...");
+        let default_config = Sso::default_config(&sso_config)?;
+        info!("Retrieving account info...");
+        let account_info_provider = AccountInfoProvider::new(&default_config);
+        info!("Retrieving session name...");
+        let session_name = session_name(sso_config.start_url.as_str());
+        let token_provider = SsoAccessTokenProvider::new(
+            &default_config,
+            session_name.as_str(),
+            &PathBuf::from(load_cache_file()),
+        )?;
+        info!("Retrieving access token...");
+        let access_token = token_provider
+            .get_access_token(&sso_config.start_url)
+            .await?;
+        info!("Retrieving sso accounts...");
+        let mut sso_accounts = account_info_provider
+            .get_account_list(&access_token)
+            .await?;
+        sso_accounts.sort();
+        let selected_account = Select::new(Prompt::SELECT_ACCOUNT, sso_accounts).prompt()?;
+        let mut roles = account_info_provider
+            .get_roles_for_account(&access_token, &selected_account)
+            .await?;
+        roles.sort();
+        let selected_role = Select::new(Prompt::SELECT_ROLE, roles).prompt()?;
+
+        // Get role credentials
+        let role_credentials = account_info_provider
+            .get_role_credentials(&selected_role, &selected_account, &access_token)
+            .await?;
+        let (mut access_key_id, mut secret_access_key, mut session_token, mut expiration) =
+            (String::new(), String::new(), String::new(), String::new());
+        let _ = role_credentials
+            .role_credentials
+            .into_iter()
+            .map(|x| {
+                access_key_id = x.access_key_id().unwrap().to_string();
+                secret_access_key = x.secret_access_key().unwrap().to_string();
+                session_token = x.session_token().unwrap().to_string();
+                expiration = x.expiration().to_string();
+            })
+            .collect::<Vec<()>>();
+        let new_profile = Text::new(Prompt::INPUT_PROFILE).prompt()?;
+
+        // Write credentials to file
+        println!("Updating credentials file using profile: {}", new_profile);
+        self.write_to_credentials_file(
+            new_profile.as_str(),
+            sso_config.clone().region.as_str(),
+            selected_account.account_id.as_str(),
+            access_key_id.as_str(),
+            secret_access_key.as_str(),
+            session_token.as_str(),
+            expiration.parse::<i64>().unwrap(),
+        )?;
+        Ok(())
     }
 }
 
