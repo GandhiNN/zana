7dd2120f667dad01d6c05165bbdf71719ddf8b58
Ngakan Gandhi <ngandhi@pmintl.net>
2025-02-28 19:40:02 +0700

    set defaults module

---
+++ src/aws/defaults.rs
@@ -0,0 +1,10 @@
+pub const AWS_SSO_PATH: &str = ".aws_sso";
+pub const CREDENTIALS_PATH: &str = ".aws_sso/credentials";
+pub const CONFIG_PATH: &str = ".aws_sso/config";
+pub const CREDENTIALS_PLACEHOLDER: &str = "[dummy_profile]
+aws_account_id=11111111111
+region=eu-west-1
+aws_secret_access_key=dummy_secret_access_key
+aws_access_key_id=dummy_access_key_id
+aws_session_token=dummy_session_token
+";
